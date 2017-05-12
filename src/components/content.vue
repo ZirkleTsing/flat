64164051b75bca2928f07374b28643af15e32af3
chang-home <835702580@qq.com>
2017-05-13 01:44:41 +0800

    header layout

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="content-wrapper">
-    content
+    <section class="green-section">
+      <div class="wrapper">
+        <h2>标题1</h2>
+        <div class="hr"></div>
+        <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+      </div>
+      <div class="img">
+        <span>pic1</span>
+        <span>pic2</span>
+        <span>pic3</span>
+        <span>pic4</span>
+      </div>
+    </section>
+    <section class="gray-section">456</section>
+    <section class="purple-section">789</section>
   </div>
 </template>
 
@@ -8,6 +22,35 @@
   export default {}
 </script>
 
-<style>
-
+<style lang="stylus">
+  .green-section
+    height: 700px
+    background: #27ecc5
+    color: white
+    .wrapper
+      padding-top: 200px
+      width: 1080px
+      margin: 0 auto
+      text-align: center
+      letter-spacing: 1px
+      h2
+        padding-bottom: 20px
+        font-weight: 700
+        font-size: 30px
+      .hr
+        height: 2px
+        margin: 0 auto
+        width: 55%
+        background: #fff
+      p
+        margin-top: 20px
+        font-size: 18px
+    .img
+      width: 1080px
+      margin: 0 auto
+      padding-top: 160px
+      text-align: center
+      span
+        margin-right: 20px
+        font-size: 20px
 </style>
--- src/components/header.vue
+++ src/components/header.vue
@@ -1,13 +1,95 @@
 <template>
   <header>
-    header
+    <div class="header-wrapper">
+      <nav>
+        <h1 id="header-info">
+          <a id="header-info-address" href="#">常小窝</a>
+        </h1>
+        <ul id="header-list">
+          <li class="list" v-for="item in navList">
+            <a :href="item.address">{{ item.text }}</a>
+          </li>
+        </ul>
+      </nav>
+      <div class="banner">
+        <div class="inner">
+          <h2 id="brand">常小窝</h2>
+          <p id="desc">Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+          <button id="banner-button">了解我</button>
+          <div class="more">更多</div>
+        </div>
+      </div>
+    </div>
   </header>
 </template>
 
 <script>
-
+  export default {
+    data () {
+      return {
+        navList: [
+          {
+            text: '首页',
+            address: '#'
+          },
+          {
+            text: '导航',
+            address: '#'
+          }
+        ]
+      }
+    }
+  }
 </script>
 
-<style>
-
+<style lang="stylus">
+  .header-wrapper
+    position: relative
+    color: #fff
+  nav
+    height: 50px
+    background: #333
+  #header-info
+    display: inline-block
+    box-sizing:border-box
+    padding: 10px 15px
+    line-height: 30px
+    font-size: 25px
+    letter-spacing: 1px
+    #header-info-address
+      color: #fff
+  #header-list
+    display: inline-block
+    float: right
+    padding: 15px 15px
+  .list
+    display: inline-block
+    font-size: 20px
+  .list a
+    margin-right: 10px
+    letter-spacing: 1px
+    color: #fff
+  .banner
+    height: 700px
+    background: #444
+    .inner
+      margin: 0 auto
+      box-sizing: border-box
+      padding-top: 160px
+      width: 300px
+      text-align: center
+      letter-spacing: 1px
+      #brand
+        display: block
+        margin-bottom: 20px
+        padding: 10px 0px
+        font-size: 35px
+        border-top: 3px solid #fff
+        border-bottom: 3px solid #fff
+      #banner-button
+        margin-top: 20px
+        width: 200px
+      .more
+        font-size: 18px
+        margin-top: 260px
 </style>
