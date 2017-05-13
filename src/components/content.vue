6ea8246bc030545bf6404a1e3ff39486f25a249c
chang-home <835702580@qq.com>
2017-05-13 15:59:18 +0800

    add content layout

--- src/components/content.vue
+++ src/components/content.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="content-wrapper">
     <section class="green-section">
-      <div class="wrapper">
+      <div class="green-wrapper">
         <h2>标题1</h2>
         <div class="hr"></div>
         <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
@@ -13,13 +13,77 @@
         <span>pic4</span>
       </div>
     </section>
-    <section class="gray-section">456</section>
-    <section class="purple-section">789</section>
+    <section class="gray-section">
+      <div class="gray-wrapper clearfix">
+        <div class="section clearfix bg1">
+          <div class="img">
+            <img :src="pic1" alt="pic01">
+          </div>
+          <div class="text">
+            <h2>标题2</h2>
+            <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+          </div>
+        </div>
+        <div class="section clearfix bg2">
+          <div class="text">
+            <h2>标题3</h2>
+            <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+          </div>
+          <div class="img">
+            <img :src="pic2" alt="pic01">
+          </div>
+        </div>
+        <div class="section clearfix bg3">
+          <div class="img">
+            <img :src="pic3" alt="pic01">
+          </div>
+          <div class="text">
+            <h2>标题4</h2>
+            <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="purple-section">
+      <div class="purple-wrapper clearfix">
+        <div class="header">
+          <h2>标题5</h2>
+          <div class="hr"></div>
+          <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+        </div>
+        <div class="cart-group">
+          <div class="card bg1">
+            <h2>又一个标题</h2>
+            <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+          </div>
+          <div class="card bg2">
+            <h2>又一个标题</h2>
+            <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+          </div>
+          <div class="card bg3">
+            <h2>又一个标题</h2>
+            <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+          </div>
+          <div class="card bg4">
+            <h2>又一个标题</h2>
+            <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data () {
+      return {
+        pic1: require('../assets/pic01.jpg'),
+        pic2: require('../assets/pic02.jpg'),
+        pic3: require('../assets/pic03.jpg')
+      }
+    }
+  }
 </script>
 
 <style lang="stylus">
@@ -27,7 +91,7 @@
     height: 700px
     background: #27ecc5
     color: white
-    .wrapper
+    .green-wrapper
       padding-top: 200px
       width: 1080px
       margin: 0 auto
@@ -51,6 +115,78 @@
       padding-top: 160px
       text-align: center
       span
-        margin-right: 20px
+        display: inline-block
+        margin-right: 50px
         font-size: 20px
+        line-height: 80px
+        width: 80px
+        height: 80px
+        background: #333
+        font-weight: 700
+        border: 1px solid #000
+        transform: rotate(45deg)
+  .gray-section
+    color: #fff
+    font-size: 0
+    .gray-wrapper
+      .section
+        .img
+          display: inline-block
+          width: 45%
+          img
+            width: 100%
+        .text
+          padding-top: 6%
+          width: 55%
+          letter-spacing: 1px
+          h2
+            padding-left: 5%
+            font-size: 30px
+            font-weight: 700
+          p
+            padding-left: 5%
+            margin-top: 20px
+            font-size: 18px
+      .section > div
+        float: left
+      .bg1,.bg3
+        background-color: #00353b
+      .bg2
+        background-color: #023f46
+  .purple-section
+    box-sizing: border-box
+    background: #554075
+    color: #fff
+    .purple-wrapper
+      width: 1080px
+      margin: 0 auto
+      padding-bottom: 70px
+      text-align: center
+      .header
+        padding-top: 120px
+        margin-bottom: 120px
+        h2
+          font-size: 30px
+          font-weight: 700
+          margin-bottom: 20px
+      .cart-group
+        width: 100%
+        .card
+          display: inline-block
+          box-sizing: border-box
+          padding-top: 100px
+          width: 50%
+          height: 300px
+          float: left
+          letter-spacing: 1px
+          h2
+            margin-bottom: 20px
+            font-size: 28px
+          p
+            padding 0 30px
+            font-size: 20px
+        .bg1, .bg4
+          background: rgba(0, 0, 0, 0.04)
+        .bg2, .bg3
+          background: rgba(0, 0, 0, 0.08)
 </style>
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,16 @@
 <template>
   <footer>
-    footer
+    <div class="footer-wrapper">
+      <div class="inner">
+        <h2>爱我就养我</h2>
+        <p>Lorem ipsum dolor sit amet, consecterur adipisicing elit.</p>
+        <button>给钱</button>
+        <button>给吃的</button>
+      </div>
+      <div class="bottom">
+
+      </div>
+    </div>
   </footer>
 </template>
 
